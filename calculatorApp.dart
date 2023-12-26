647a405b16e0febd1d46965211eb0f45e52419d4
Thomas Mutisya Mutua <mutisyathomas0@gmail.com>
2023-12-26 16:28:35 +0300

    Add files via upload

---
+++ calculatorApp.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+double inputNumber() {
+  print("Enter a number for operation: ");
+  double number = double.parse(stdin.readLineSync()!);
+  return number;
+}
+
+String operandChoice() {
+  print("Enter operand of your choice i.e '+','-','*','/' : ");
+  String choice = stdin.readLineSync()!;
+  return choice;
+}
+
+void main() {
+  double num1 = inputNumber();
+  double num2 = inputNumber();
+  String operand = operandChoice();
+  double answer = 0.0;
+
+  if (operand == '+') {
+    answer = num1 + num2;
+    print("The sum of $num1 and $num2 is : $answer");
+  } else if (operand == '-') {
+    answer = num1 - num2;
+    print("The diffrence between $num1 and $num2 is: $answer");
+  } else if (operand == '*') {
+    answer = num1 * num2;
+    print("The multiplication of $num1 and $num2 is : $answer");
+  } else if (operand == '/') {
+    answer = num1 / num2;
+    print("$num1 divided by $num2 is : $answer");
+  } else {
+    print("Invalid choice of operand,try again!!!!");
+  }
+}
